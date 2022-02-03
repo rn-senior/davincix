9d963877dc84b5208249f6171dca666534ac2e84
rn-senior <87538599+rn-senior@users.noreply.github.com>
2022-02-03 10:53:50 +0800

    updates

---
+++ contact/contact.php
@@ -0,0 +1,107 @@
+<?php
+	if($_POST) {
+	    $visitor_name = "";
+	    $visitor_email = "";
+	    $visitor_message = "";
+	    $email_body = "<div>";
+	     
+	    if(isset($_POST['name'])) {
+	        $visitor_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+	        $email_body .= "<div>
+	                           <label><b>Visitor Name:</b></label>&nbsp;<span>".$visitor_name."</span>
+	                        </div>";
+	    }
+
+	    if(isset($_POST['email'])) {
+	        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+	        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+	        $email_body .= "<div>
+	                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$visitor_email."</span>
+	                        </div>";
+	    }
+	     
+	    if(isset($_POST['subject'])) {
+	        $visitor_message = htmlspecialchars($_POST['subject']);
+	        $email_body .= "<div>
+	                           <label><b>Visitor Message:</b></label>
+	                           <div>".$visitor_message."</div>
+	                        </div>";
+	    }
+
+	    $recipient = "jin@cloudwinnings.com";
+	     
+	    $email_body .= "</div>";
+
+	    $headers  = 'MIME-Version: 1.0' . "\r\n"
+	    .'Content-type: text/html; charset=utf-8' . "\r\n"
+	    .'From: ' . $visitor_email . "\r\n";
+	     
+	    if(mail("pooh199788@gmail.com", "New Project Request", $email_body, $headers)) {
+	        $status = true;
+	    } else {
+	        $status = false;
+	    }
+?>
+<?php 
+   include("../partial/header.php");
+?>
+	<main>
+         <section class="block block--our-values">
+            <div class="container">
+               <div class="row" style="margin-top: 50px;">
+                  <div class="col-sm-8 col-sm-offset-2 col-md-12 col-md-offset-0">
+                     <div class="section-heading center-grid section-heading--large-margin">
+                        <h4 class="section-heading__headline">Contact us</h4>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="container">
+               <div class="row" style="margin-top: 50px;margin-bottom: 50px;">
+                  <div class = "col-md-12">
+                     <h3 class="list-with-image__headline x-title-2 text-center">
+	        			<p>Thank you for contacting us, <?=$visitor_name?>. You will get a reply within 24 hours.</p>
+                     </h3>
+                  </div>
+               </div>
+            </div>
+         </section>
+      </main>
+<?php 
+   include("../partial/footer.php");
+?>
+<?php
+	} else {
+?>
+	
+<?php 
+   include("../partial/header.php");
+?>
+	<main>
+         <section class="block block--our-values">
+            <div class="container">
+               <div class="row" style="margin-top: 50px;">
+                  <div class="col-sm-8 col-sm-offset-2 col-md-12 col-md-offset-0">
+                     <div class="section-heading center-grid section-heading--large-margin">
+                        <h4 class="section-heading__headline">Contact us</h4>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="container">
+               <div class="row" style="margin-top: 50px;margin-bottom: 50px;">
+                  <div class = "col-md-12">
+                     <h3 class="list-with-image__headline x-title-2 text-center">
+	        			<p>Sorry, we are unable to send email, please try again later!</p>
+                     </h3>
+                  </div>
+               </div>
+            </div>
+         </section>
+      </main>
+<?php 
+   include("../partial/footer.php");
+?>
+<?php 		
+	}
+?>
--- contact/index.php
+++ contact/index.php
@@ -1,6 +1,40 @@
 <?php 
    include("../partial/header.php");
 ?>
+<style>
+   input[type=text], select, textarea {
+     width: 100%; /* Full width */
+     padding: 12px; /* Some padding */ 
+     border: 1px solid #ccc; /* Gray border */
+     border-radius: 4px; /* Rounded borders */
+     box-sizing: border-box; /* Make sure that padding and width stays in place */
+     margin-top: 6px; /* Add a top margin */
+     margin-bottom: 16px; /* Bottom margin */
+     resize: vertical /* Allow the user to vertically resize the textarea (not horizontally) */
+   }
+
+   /* Style the submit button with a specific background color etc */
+   input[type=submit] {
+     background-color: #04AA6D;
+     color: white;
+     padding: 12px 20px;
+     border: none;
+     border-radius: 4px;
+     cursor: pointer;
+   }
+
+   /* When moving the mouse over the submit button, add a darker green color */
+   input[type=submit]:hover {
+     background-color: #45a049;
+   }
+
+   /* Add a background color and some padding around the form */
+   .contact-container {
+     border-radius: 5px;
+     background-color: #f2f2f2;
+     padding: 20px;
+   }
+</style>
       <main>
          <section class="block block--our-values">
             <div class="container">
@@ -18,12 +52,28 @@
                      <h3 class="list-with-image__headline x-title-2">
                         Phone: 248-875-7114
                      </h3>
+                     <h3 class="list-with-image__headline x-title-2">
+                        E-mail: contact@damundi.com
+                     </h3>
                   </div>
 
                   <div class = "col-md-6">
-                     <h3 class="member__name x-title-3">
-                        The team is open for your ideas, questions and needs Our clients get the superior results when a short-term acquaintance turns into a long-term collaboration.
-                     </h3>
+                     <div class="contact-container">
+                       <form action="contact.php" method="post">
+
+                         <label for="fname">First Name</label>
+                         <input type="text" id="fname" name="name" required placeholder="Your name..">
+
+                         <label for="lname">Email</label>
+                         <input type="text" id="email" name="email" required placeholder="Your email..">
+
+                         <label for="subject">About Project</label>
+                         <textarea id="subject" name="subject" required placeholder="Write about your project" style="height:200px"></textarea>
+
+                         <input type="submit" value="Submit">
+
+                       </form>
+                     </div>
                   </div>
                </div>
             </div>

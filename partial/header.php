9e096d1de0695d4dbe69baf1719fab30a111d492
rn-senior <87538599+rn-senior@users.noreply.github.com>
2022-02-04 12:33:48 +0800

    Update header.php

--- partial/header.php
+++ partial/header.php
@@ -2,8 +2,8 @@
 <html lang="en">
    <head>
       <?php
-        // $url = "https://damundi.com";
-        $url = "http://localhost/davincix";
+        $url = "https://damundi.com";
+        // $url = "http://localhost/davincix";
       ?>
       <meta charset="utf-8">
       <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -21,6 +21,26 @@
       <link rel="shortcut icon" type="image/x-icon" href=<?=$url."/static/favicons/favicon.ico"?>>
       <link rel="icon" type="image/x-icon" href=<?=$url."/static/favicons/favicon.ico"?>>
       <link type="text/css" href="<?=$url?>/static/bundles/base.a468487c7caa119f3897.css" rel="stylesheet" />
+      
+      <meta name="facebook-domain-verification" content="72bej18wefuuyizevbkrk4rmwrgy5j" />
+
+      <!-- Meta Pixel Code -->
+        <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '458076766044664');
+        fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=458076766044664&ev=PageView&noscript=1"
+        /></noscript>
+        <!-- End Meta Pixel Code -->
    </head>
    <style>
       .sr-only{
